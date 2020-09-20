1bc84a37f76f87893a17d75ba093229b4e99d5fc
yiyb0603 <yiyb0603@gmail.com>
2020-09-21 08:17:44 +0900

    [FIX] classnames to cx

--- src/components/Common/SelectBox/SelectBox.tsx
+++ src/components/Common/SelectBox/SelectBox.tsx
@@ -1,20 +1,27 @@
 import React, { ReactNode, ChangeEvent } from 'react';
+import classNames from 'classnames';
+import { ClassNamesFn } from 'classnames/types';
 
-import './SelectBox.scss';
+const style = require('./SelectBox.scss');
+const cx: ClassNamesFn = classNames.bind(style);
 
 interface ISelectBox {
-  children?: ReactNode;
-  className?: string;
-  style?: object;
-  onChange: (e: ChangeEvent<HTMLSelectElement>) => void;
+	children?: ReactNode;
+	className?: string;
+	style?: Object;
+	onChange: (e: ChangeEvent<HTMLSelectElement>) => void;
 }
 
 const SelectBox = ({ children, className, style, onChange }: ISelectBox) => {
-  return (
-    <select onChange={onChange} className={`select-box ${className}`} style={style}>
-      {children}
-    </select>
-  );
-}
+	return (
+		<select
+			onChange={onChange}
+			className={cx(`select-box ${className}`)}
+			style={style}
+		>
+			{children}
+		</select>
+	);
+};
 
 export default SelectBox;
